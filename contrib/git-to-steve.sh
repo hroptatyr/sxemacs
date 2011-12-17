23cf82a7fa5b37d7e31bf1f430422ecbcc7146e8
Nelson Ferreira <nelson.ferreira@ieee.org>
2011-12-16 23:51:26 -0500

    Add check for user email and name setup
    Signed-off-by: Nelson Ferreira <nelson.ferreira@ieee.org>

--- contrib/git-to-steve.sh
+++ contrib/git-to-steve.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 #
 # 
+USER_EMAIL=$(git config user.email)
+if [ -z "$USER_EMAIL" ]; then
+    echo "You need to setup your email address with:"
+    echo "    git config user.email <your email address>"
+    exit 1
+fi
+USER_NAME=$(git config user.name)
+if [ -z "$USER_NAME" ]; then
+    echo "You need to configure git with your name:"
+    echo "    git config user.name \"John Doe\""
+    exit 1
+fi
 git branch --track for-steve origin/master
 git checkout for-steve
