hroptatyr/sxemacs

commit cb837c1cca8929b343e620e362060a96743d550f

SECURE_CODING: Use snprintf/write_fmt_str instead of sprintf
	* etc/tests/external-widget/test-ew-motif.c (ScaleValueChangedCB):
	Use snprintf instead of sprintf.
	* etc/tests/external-widget/test-ew-motif.c (main): ditto.
	* etc/tests/external-widget/test-ew-motif.c (main): ditto.
	* etc/tests/external-widget/test-ew-xlib.c (draw_text): ditto
	* lib-src/gnuclient.c (tell_emacs_to_resume): ditto.
	* lib-src/gnuclient.c (main): ditto.
	* lib-src/gnuserv.c (ipc_init): ditto.
	* lib-src/gnuserv.c (unix_init): ditto.
	* lib-src/gnuslib.c (connect_to_ipc_server): ditto.
	* lib-src/gnuslib.c (connect_to_unix_server): ditto.
	* lib-src/make-po.c (buf_print): ditto.
	* lib-src/movemail.c (lock_dot): ditto.
	* lib-src/pop.c (pop_open): ditto.
	* lib-src/profile.c (get_time): ditto.
	* lib-src/yow.c (main): ditto.
	* src/bytecode.c (invalid_byte_code_error): ditto.
	* src/extents.c (print_extent_1): ditto.
	* src/m/cydra5.h (PTY_NAME_SPRINTF): ditto.
	* src/m/cydra5.h (PTY_TTY_NAME_SPRINTF): ditto.
	* src/media/media-ffmpeg.c (media_ffmpeg_streaminfo): ditto.
	* src/media/sound-hpplay.c (player_error_internal): ditto.
	* src/media/sound-sunplay.c (init_device): ditto.
	* src/media/sound-sunplay.c (play_sound_file): ditto.
	* src/media/sound-sunplay.c (play_sound_data): ditto.
	* src/media/sound-sunplay.c (sound_native_audio_init): ditto.
	* src/process.c (make_process_internal): ditto.
	* src/ui/Gtk/menubar-gtk.c (menu_descriptor_to_widget_1): ditto.

	* src/print.c: fix comment

	* modules/ase/ase-resclass.h (resc_rng_to_string): check return of
	snprintf.
	* modules/ase/ase-resclass.c (_ase_resc_rng_to_string): ditto.
	* modules/ase/ase-resclass.c (_ase_resc_elm_to_string): ditto.
	* src/media/sound-jack.c (sound_jack_subthread_create): ditto.

	* src/effi.c (WRITE_FMT_STRING): new macro to use
	write_fmt_string.

	* src/lisp.h (PRIVATE_EXTERNAL_LIST_LOOP_6): Finally fix the
	computed value not used warning.

	* src/events/events.c (print_event): remove unneeded buf

lib-src/yow.c
@@ -44,15 +44,16 @@ int main(int argc, char *argv[])
 	} else
 #ifdef PATH_DATA
 #ifdef vms
-		sprintf(file, "%s%s", PATH_DATA, YOW_FILE);
+		int sz = snprintf(file, sizeof(file), "%s%s", PATH_DATA, YOW_FILE);
 #else
-		sprintf(file, "%s/%s", PATH_DATA, YOW_FILE);
+	        int sz = snprintf(file, sizeof(file), "%s/%s", PATH_DATA, YOW_FILE);
 #endif
+		assert(sz>=0 && sz<sizeof(file));
 #else				/* !PATH_DATA */
 	{
 		fprintf(stderr,
 			"%s: the location of the \"%s\" file was not supplied at compile-time.\n\
-        You must supply it with the -f command-line option.\n",
+You must supply it with the -f command-line option.\n",
 			argv[0], YOW_FILE);
 		exit(1);
 	}

etc/tests/external-widget/test-ew-motif.c
@@ -17,7 +17,8 @@ void ScaleValueChangedCB(Widget scale, XtPointer app_data,
 	char labelarr[10];
 	XmString labelstr;
 #if 0
-	sprintf(labelarr, "%d", xms->value);
+	int sz = snprintf(labelarr, sizeof(labelarr), "%d", xms->value);
+	assert(sz>=0 && sz<sizeof(labelarr));
 	labelstr = XmStringCreateLocalized(labelarr);
 	XtVaSetValues(label, XmNlabelString, labelstr, NULL);
 	XmStringFree(labelstr);
@@ -68,7 +69,8 @@ main(int argc, char **argv)
 	pushbutton = XmCreatePushButton(paned, "pushbutton", NULL, 0);
 	text = XmCreateText(paned, "text", NULL, 0);
 	for (i = 0; i < no_ews; i++) {
-		sprintf(buf, "extcli%d", i);
+		int sz = snprintf(buf, sizeof(buf), "extcli%d", i);
+		assert(sz>=0 && sz < sizeof(buf));
 		emacscli[i] =
 		    XtVaCreateWidget(buf, externalClientWidgetClass, paned,
 				     XmNwidth, 500, XmNheight, 200,
@@ -93,7 +95,9 @@ main(int argc, char **argv)
 
 		strcpy(labarr, "window:");
 		for (i = 0; i < no_ews; i++) {
-			sprintf(tmpbuf, " %d", XtWindow(emacscli[i]));
+			int sz = snprintf(tmpbuf, sizeof(tmpbuf), 
+					  " %d", XtWindow(emacscli[i]));
+			assert(sz>=0 && sz<sizeof(tmpbuf));
 			strcat(labarr, tmpbuf);
 		}
 		lab = XmStringCreateLocalized(labarr);

etc/tests/external-widget/test-ew-xlib.c
@@ -244,8 +244,8 @@ XFontStruct *font_info;
 	XSetDashes(display, *gc, dash_offset, dash_list, list_length);
 }
 
-load_font(font_info)
-XFontStruct **font_info;
+void
+load_font(XFontStruct **font_info)
 {
 	char *fontname = "9x15";
 
@@ -256,11 +256,8 @@ XFontStruct **font_info;
 	}
 }
 
-draw_text(win, gc, font_info, win_width, win_height)
-Window win;
-GC gc;
-XFontStruct *font_info;
-unsigned int win_width, win_height;
+void
+draw_text(Window win, GC gc, XFontStruct *font_info, unsigned int win_width, unsigned int win_height)
 {
 	char *string1 = "Hi! I'm a window, who are you?";
 	char *string2 = "To terminate program; Press any key";
@@ -271,6 +268,7 @@ unsigned int win_width, win_height;
 	char cd_height[50], cd_width[50], cd_depth[50];
 	int font_height;
 	int initial_y_offset, x_offset;
+	int sz;
 
 	/* need length for both XTextWidth and XDrawString */
 	len1 = strlen(string1);
@@ -293,12 +291,17 @@ unsigned int win_width, win_height;
 		    (int)(win_height - font_height), string3, len3);
 
 	/* copy numbers into string variables */
-	(void)sprintf(cd_height, " Height - %d pixels",
+	sz = snprintf(cd_height, sizeof(cd_height), " Height - %d pixels",
 		      DisplayHeight(display, screen_num));
-	(void)sprintf(cd_width, " Width  - %d pixels",
+	assert(sz>=0 && sz<sizeof(cd_height));
+
+	sz = snprintf(cd_width, sizeof(cd_width), " Width  - %d pixels",
 		      DisplayWidth(display, screen_num));
-	(void)sprintf(cd_depth, " Depth  - %d plane(s)",
+	assert(sz>=0 && sz<sizeof(cd_width));
+
+	sz = snprintf(cd_depth, sizeof(cd_depth), " Depth  - %d plane(s)",
 		      DefaultDepth(display, screen_num));
+	assert(sz>=0 && sz<sizeof(cd_depth));
 
 	/* reuse these for same purpose */
 	len4 = strlen(string4);

lib-src/gnuclient.c
@@ -58,6 +58,7 @@ char gnuserv_version[] = "gnuclient version " GNUSERV_VERSION;
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sysfile.h>
+#include <assert.h>
 
 #ifdef HAVE_STRING_H
 #include <string.h>
@@ -91,6 +92,7 @@ void initialize_signals(void);
 static void tell_emacs_to_resume(int sig)
 {
 	char buffer[GSERV_BUFSZ + 1];
+	int sz;
 	int s;			/* socket / msqid to server */
 	int connect_type;	/* CONN_UNIX, CONN_INTERNET, or
 				   ONN_IPC */
@@ -103,8 +105,9 @@ static void tell_emacs_to_resume(int sig)
 
 	connect_type = make_connection(NULL, 0, &s);
 
-	sprintf(buffer, "(gnuserv-eval '(resume-pid-console %d))",
-		(int)getpid());
+	sz = snprintf(buffer, sizeof(buffer), "(gnuserv-eval '(resume-pid-console %d))",
+		      (int)getpid());
+	assert(sz>=0 && sz<sizeof(buffer));
 	send_string(s, buffer);
 
 #ifdef SYSV_IPC
@@ -309,6 +312,7 @@ int main(int argc, char *argv[])
 	char buffer[GSERV_BUFSZ + 1];	/* buffer to read pid */
 	char result[GSERV_BUFSZ + 1];
 	int i;
+	int sz, msz;
 
 #ifdef INTERNET_DOMAIN_SOCKETS
 	memset(remotepath, 0, sizeof(remotepath));
@@ -442,8 +446,9 @@ int main(int argc, char *argv[])
 #else
 		connect_type = make_connection(NULL, 0, &s);
 #endif
-		sprintf(command, "(gnuserv-eval%s '(progn ",
-			quick ? "-quickly" : "");
+		sz = snprintf(command, sizeof(command), "(gnuserv-eval%s '(progn ",
+			 quick ? "-quickly" : "");
+		assert(sz>=0 && sz<sizeof(command));
 		send_string(s, command);
 		if (load_library) {
 			send_string(s, "(load-library ");
@@ -477,8 +482,10 @@ int main(int argc, char *argv[])
 #else
 		connect_type = make_connection(NULL, 0, &s);
 #endif
-		sprintf(command, "(gnuserv-eval%s '(progn ",
-			quick ? "-quickly" : "");
+		sz = snprintf(command, sizeof(command),
+			      "(gnuserv-eval%s '(progn ",
+			      quick ? "-quickly" : "");
+		assert(sz>=0 && sz<sizeof(command));
 		send_string(s, command);
 
 		while ((nb = read(fileno(stdin), buffer, GSERV_BUFSZ - 1)) > 0) {
@@ -575,23 +582,27 @@ int main(int argc, char *argv[])
 
 		if (suppress_windows_system) {
 			char *term = getenv("TERM");
+			int sz;
 			if (!term) {
 				fprintf(stderr, "%s: unknown terminal type\n",
 					progname);
 				exit(1);
 			}
-			sprintf(command,
-				"(gnuserv-edit-files '(tty %s %s %d) '(",
-				clean_string(tty), clean_string(term),
-				(int)getpid());
+			sz = snprintf(command, sizeof(command),
+				      "(gnuserv-edit-files '(tty %s %s %d) '(",
+				      clean_string(tty), clean_string(term),
+				      (int)getpid());
+			assert(sz>=0 && sz<sizeof(command));
 		} else {	/* !suppress_windows_system */
 
 			if (0) ;
 #ifdef HAVE_X_WINDOWS
-			else if (display)
-				sprintf(command,
-					"(gnuserv-edit-files '(x %s) '(",
-					clean_string(display));
+			else if (display) {
+				int sz = snprintf(command, sizeof(command),
+						  "(gnuserv-edit-files '(x %s) '(",
+						  clean_string(display));
+				assert(sz>=0 && sz<sizeof(command));
+			}
 #endif
 #ifdef HAVE_GTK
 			else if (display)
@@ -616,24 +627,28 @@ int main(int argc, char *argv[])
 			}
 			filename_expand(fullpath, argv[i], sizeof(fullpath));
 #ifdef INTERNET_DOMAIN_SOCKETS
-			path =
-			    (char *)malloc(strlen(remotepath) +
-					   strlen(fullpath) + 1);
-			sprintf(path, "%s%s", remotepath, fullpath);
+			msz = strlen(remotepath) + strlen(fullpath) + 1;
+			path = (char *)malloc(msz);
+			sz = snprintf(path, msz, "%s%s", remotepath, fullpath);
+			assert(sz>=0 && sz<msz);
 #else
 			path = my_strdup(fullpath);
 #endif
-			sprintf(command, "(%d . %s)", starting_line,
-				clean_string(path));
+			sz = snprintf(command, sizeof(command),
+				      "(%d . %s)", starting_line,
+				      clean_string(path));
+			assert(sz>=0 && sz<sizeof(command));
 			send_string(s, command);
 			free(path);
 		}		/* for */
-
-		sprintf(command, ")%s%s",
-			(quick
-			 || (nofiles
-			     && !suppress_windows_system)) ? " 'quick" : "",
-			view ? " 'view" : "");
+		
+		sz = snprintf(command, sizeof(command),
+			      ")%s%s",
+			      (quick
+			       || (nofiles
+				   && !suppress_windows_system)) ? " 'quick" : "",
+			      view ? " 'view" : "");
+		assert(sz>=0 && sz<sizeof(command));
 		send_string(s, command);
 		send_string(s, ")");
 

lib-src/gnuserv.c
@@ -30,6 +30,7 @@
  */
 
 #include "gnuserv.h"
+#include <assert.h>
 
 char gnuserv_version[] = "gnuserv version" GNUSERV_VERSION;
 
@@ -128,7 +129,8 @@ void ipc_init(struct msgbuf **msgpp)
 	key_t key;		/* messge key */
 	char buf[GSERV_BUFSZ];	/* pathname for key */
 
-	sprintf(buf, "%s/gsrv%d", tmpdir, (int)geteuid());
+	int sz = snprintf(buf, sizeof(buf),"%s/gsrv%d", tmpdir, (int)geteuid());
+	assert(sz>=0 && sz<sizeof(buf));
 	creat(buf, 0600);
 	key = ftok(buf, 1);
 
@@ -703,6 +705,7 @@ static int unix_init(void)
 	int ls;			/* socket descriptor */
 	struct sockaddr_un server;	/* unix socket address */
 	socklen_t bindlen;
+	int sz;
 
 	if ((ls = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
 		perror(progname);
@@ -713,7 +716,9 @@ static int unix_init(void)
 	/* if */
 	/* Set up address structure for the listen socket. */
 #ifdef HIDE_UNIX_SOCKET
-	sprintf(server.sun_path, "%s/gsrvdir%d", tmpdir, (int)geteuid());
+	sz = snprintf(server.sun_path, sizeof(server.sun_path),
+		      "%s/gsrvdir%d", tmpdir, (int)geteuid());
+	assert(sz>=0 && sz<sizeof(server.sun_path));
 	if (mkdir(server.sun_path, 0700) < 0) {
 		/* assume it already exists, and try to set perms */
 		if (chmod(server.sun_path, 0700) < 0) {
@@ -726,7 +731,9 @@ static int unix_init(void)
 	strcat(server.sun_path, "/gsrv");
 	unlink(server.sun_path);	/* remove old file if it exists */
 #else				/* HIDE_UNIX_SOCKET */
-	sprintf(server.sun_path, "%s/gsrv%d", tmpdir, (int)geteuid());
+	sz = snprintf(server.sun_path, sizeof(server.sun_path),
+		      "%s/gsrv%d", tmpdir, (int)geteuid());
+	assert(sz>=0 && sz<sizeof(server.sun_path));
 	unlink(server.sun_path);	/* remove old file if it exists */
 #endif				/* HIDE_UNIX_SOCKET */
 

lib-src/gnuslib.c
@@ -130,7 +130,9 @@ static int connect_to_ipc_server(void)
 	key_t key;		/* message key */
 	char buf[GSERV_BUFSZ + 1];	/* buffer for filename */
 
-	sprintf(buf, "%s/gsrv%d", tmpdir, (int)geteuid());
+	int sz = snprintf(buf, sizeof(buf), 
+			  "%s/gsrv%d", tmpdir, (int)geteuid());
+	assert(sz>=0 && sz<sizeof(buf));
 	creat(buf, 0600);
 	if ((key = ftok(buf, 1)) == -1) {
 		perror(progname);
@@ -245,6 +247,7 @@ static int connect_to_unix_server(void)
 {
 	int s;			/* connected socket descriptor */
 	struct sockaddr_un server;	/* for unix connections */
+	int sz;
 
 	if ((s = socket(AF_UNIX, SOCK_STREAM, 0)) < 0) {
 		perror(progname);
@@ -254,10 +257,13 @@ static int connect_to_unix_server(void)
 
 	server.sun_family = AF_UNIX;
 #ifdef HIDE_UNIX_SOCKET
-	sprintf(server.sun_path, "%s/gsrvdir%d/gsrv", tmpdir, (int)geteuid());
+	sz = snprintf(server.sun_path, sizeof(server.sun_path),
+		      "%s/gsrvdir%d/gsrv", tmpdir, (int)geteuid());
 #else				/* HIDE_UNIX_SOCKET */
-	sprintf(server.sun_path, "%s/gsrv%d", tmpdir, (int)geteuid());
+	sz = snprintf(server.sun_path, sizeof(server.sun_path),
+		      "%s/gsrv%d", tmpdir, (int)geteuid());
 #endif				/* HIDE_UNIX_SOCKET */
+	assert(sz>=0 && sz<sizeof(server.sun_path));
 	if (connect(s, (struct sockaddr *)&server, strlen(server.sun_path) + 2)
 	    < 0) {
 		perror(progname);

lib-src/make-po.c
@@ -274,13 +274,14 @@ int buf_putc(buffer_struct * buffer, int c)
 */
 int buf_print(buffer_struct * buffer, const char *s)
 {
-	register int len;
+	register int len, sz, msz = buffer->size - buffer->index;
 
 	len = strlen(s);
-	if (buffer->index + len >= buffer->size)
+	if ( len >= msz)
 		return -1;
 
-	sprintf(&(buffer->buf[buffer->index]), s);
+	sz = snprintf(&(buffer->buf[buffer->index]), msz, "%s", s);
+	assert(sz>=0 && sz<msz);
 	buffer->index += len;
 	return len;
 }

lib-src/movemail.c
@@ -80,6 +80,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "../src/regex.h"
 #endif
 
+#include <assert.h>
+
 extern char *optarg;
 extern int optind, opterr;
 
@@ -594,10 +596,12 @@ static void lock_dot(char *filename)
 #endif
 
 		if (desc < 0) {
-			char *message = (char *)xmalloc(strlen(tempname) + 50);
-			sprintf(message,
-				"%s--see source file lib-src/movemail.c",
-				tempname);
+			int msz = strlen(tempname) + 50;
+			char *message = (char *)xmalloc(msz);
+			int sz = snprintf(message, msz,
+					  "%s--see source file lib-src/movemail.c",
+					  tempname);
+			assert(sz>=0 && sz < msz);
 			pfatal_with_name(message);
 		}
 		close(desc);

commit 8f8568a46c509b49fca34780b50acf720c3e3abe

fix yow.c, correct else scope (broken since cb837c1c)

lib-src/yow.c
@@ -41,7 +41,7 @@ int main(int argc, char *argv[])
 	if (argc > 2 && !strcmp(argv[1], "-f")) {
 		strncpy(file, argv[2], sizeof(file)-1);
 		file[sizeof(file)-1]='\0';
-	} else
+	} else {
 #ifdef PATH_DATA
 #ifdef vms
 		int sz = snprintf(file, sizeof(file), "%s%s", PATH_DATA, YOW_FILE);
@@ -50,14 +50,13 @@ int main(int argc, char *argv[])
 #endif
 		assert(sz>=0 && sz<sizeof(file));
 #else				/* !PATH_DATA */
-	{
 		fprintf(stderr,
 			"%s: the location of the \"%s\" file was not supplied at compile-time.\n\
 You must supply it with the -f command-line option.\n",
 			argv[0], YOW_FILE);
 		exit(1);
-	}
 #endif
+	}
 
 	if ((fp = fopen(file, "r")) == NULL) {
 		perror(file);