7f35191af2879c1993a1cde9780863bd5ba2cf9c
Nelson Ferreira <nelson.ferreira@ieee.org>
2012-01-12 08:27:26 -0500

    Fix switch warnings in media
    	* src/media/media.c: New Qunavailable symbol.
    
    	* src/media/media.c (Fmedia_properties): Default driver to
    	Qunavailable. Add case labels for Xine and Gstreamer.
    
    	* src/media/media.c (syms_of_media): init Qunavailable.
    
    Signed-off-by: Nelson Ferreira <nelson.ferreira@ieee.org>

--- src/media/media.c
+++ src/media/media.c
@@ -50,6 +50,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 Lisp_Object Qmedia_streamp;
 Lisp_Object Qunknown;
+Lisp_Object Qunavailable;
 /* media property syms */
 Lisp_Object Qdemux, Qcodec, Qnchannels, Qsamplerate;
 Lisp_Object Qbitrate, Qabitrate, Qvbitrate;
@@ -991,6 +992,7 @@ in the result alist.
 		break;
 	}
 
+	__add_prop(&resdl, Qdriver, Qunavailable);
 	switch (media_stream_driver(ms)) {
 	case MDRIVER_INTERNAL:
 		__add_prop(&resdl, Qdriver, Qinternal);
@@ -1017,6 +1019,12 @@ in the result alist.
 #ifdef HAVE_SOX
 		__add_prop(&resdl, Qdriver, Qsox);
 #endif
+		break;
+
+	case MDRIVER_XINE:
+		break;
+
+	case MDRIVER_GSTREAMER:
 		break;
 
 	case MDRIVER_UNKNOWN:
@@ -1466,6 +1474,7 @@ void syms_of_media(void)
 	defsymbol(&Qsndfile, "sndfile");
 #endif
 	defsymbol(&Qunknown, "unknown");
+	defsymbol(&Qunavailable, "unavailable");
 }
 
 void vars_of_media(void)
