abd5ba930f3eff1a60d7a1380a1fa3760e2ed94c
Nelson Ferreira <nelson.ferreira@ieee.org>
2012-01-14 22:45:25 -0500

    Fix build
    	* src/buffer.h (TO_EXTERNAL_FORMAT): Move assertion up so that
    	macro can be used as single statement.
    
    	* src/buffer.h (TO_INTERNAL_FORMAT): Ditto.
    
    Signed-off-by: Nelson Ferreira <nelson.ferreira@ieee.org>

--- src/buffer.h
+++ src/buffer.h
@@ -1099,8 +1099,8 @@ do {										\
 				  dfc_simplified_sink_type,   &dfc_sink);	\
 										\
   DFC_##sink_type##_USE_CONVERTED_DATA (sink);					\
-} while (0);								        \
-assert((sink)!=NULL)
+  assert((sink)!=NULL)							        \
+} while (0)
 
 #define TO_INTERNAL_FORMAT(source_type, source, sink_type, sink, coding_system)	\
 do {										\
@@ -1131,8 +1131,8 @@ do {										\
 				  dfc_simplified_sink_type,   &dfc_sink);	\
 										\
   DFC_##sink_type##_USE_CONVERTED_DATA (sink);					\
-} while (0);								        \
-assert((sink)!=NULL)
+  assert((sink)!=NULL)							        \
+} while (0)
 
 #ifdef FILE_CODING
 #define DFC_CONVERT_TO_EXTERNAL_FORMAT dfc_convert_to_external_format
